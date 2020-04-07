95429a0401a546d1f47aaff63ea083711aa9e16d
Leonid <59738956+DoloreMentale@users.noreply.github.com>
2020-04-07 08:38:17 +0400

    Auto-generated commit

---
+++ app/cent/cent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cent',
+  templateUrl: './cent.component.html',
+  styleUrls: ['./cent.component.css']
+})
+export class CentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
